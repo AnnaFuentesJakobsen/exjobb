dc1be7e6f6a6f69ac46b2f391d1de30ed55dec83
Anna Fuentes Jakobsen <annafjakobsen@gmail.com>
2017-02-07 13:01:56 +0100

    added comments to game.js

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ var ctx = myCanvas.getContext('2d');
 var x = 20;
 var speed = 0;
 
+
+// Game-loop
 function gameTick() {
 	clear();
 	fill();
@@ -14,15 +16,20 @@ function init() {
 	var intervalID = window.setInterval(gameTick, 1000);
 }
 
+// Rensar canvas
 function clear() {
 	ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
 } 
+
+// Fyller i rektangel
 
 function fill() {
 	ctx.fillRect(x, 20, 100, 100);
 
 }
-
+// Sätter en event listener på keydown 
+// om man trycker ner D knappen så rör sig rutan åt höger
+// annars åker den åt vänster
 window.addEventListener('keydown', doKeyDown, true);
 
 function doKeyDown(e) {
